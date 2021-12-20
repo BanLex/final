942e733ca448224a305058f8067f95dfa2e7f6f4
Alexey Shabanov <banlex@yandex.ru>
2021-12-20 20:59:31 +0000

    add preinstall script

---
+++ pre_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -x
+sudo apt-get update -y
+sudo apt-get install -y git
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub
+git config --global user.name "Alexey Shabanov"
+git config --global user.email "banlex@yandex.ru"
+echo 'git clone git@github.com:BanLex/final.git'
