86deeac82d77d4891a1d3ae4e8d9f1e3419290f6
Alexey Shabanov <banlex@yandex.ru>
2021-12-20 21:49:51 +0000

    add webapp and modify preinstall

--- pre_install.sh
+++ pre_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 sudo apt-get update -y
-sudo apt-get install -y git
+sudo apt-get install -y git python3-pip python3-venv
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub
 git config --global user.name "Alexey Shabanov"
