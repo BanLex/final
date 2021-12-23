01db15a139cd665fd68a1edd115fa45474e763e1
BanLex <92094117+BanLex@users.noreply.github.com>
2021-12-23 10:51:03 +0300

    Update pre_install.sh

--- pre_install.sh
+++ pre_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -x
 sudo apt-get update -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository --yes --update ppa:ansible/ansible -y
 sudo apt-get install -y git ansible
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub
